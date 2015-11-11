c6ae2e8e96b3f86bb06cd0bb371c921c9ed29e4f
sundream <linguanglianglgl@gmail.com>
2015-11-11 20:41:32 +0800

    fixbug

--- script/data/init.lua
+++
@@ -1,6 +0,0 @@
-require "script.data.data_award"
-require "script.data.data_Huodong"
-require "script.data.data_team"
-require "script.data.data_task_shimen"
-require "script.data.data_task_ctrl"
-require "script.data.data_formula
--- script/test/test_award.lua
+++
@@ -1,6 +0,0 @@
-
-local function test(pid)
-	local player = playermgr.getplayer(pid)
-end
-
-return test
